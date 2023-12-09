5469e0aa9fa1fc88a977ed2f6ca52a83f3c6660a
Gholibjon <gholibjonmadiyarov@gmail.com>
2023-12-09 16:07:03 +0500

    Created

---
+++ Bee.Timer/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Bee.Timer
+{
+    public class Timer
+    {
+        public static void run(int ms, Action callback = null)
+        {
+            if (!(ms > 0))
+                throw new ArgumentException();
+
+            new Thread(() => {
+                Thread.Sleep(ms);
+
+                if (callback != null)
+                    callback();
+            })
+            { IsBackground = true }.Start();
+        }
+    }
+}
