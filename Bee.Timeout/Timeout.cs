46e54b674102293fe1315920170838061ea1db40
Gholibjon <gholibjonmadiyarov@gmail.com>
2024-01-22 16:08:29 +0500

    Updated
    fixed and improved

--- Bee.Timeout/Timeout.cs
+++ Bee.Timeout/Timeout.cs
@@ -5,6 +5,9 @@ namespace Bee.Timeout
 {
     public class Timeout
     {
+        DateTime dt;
+        long milliseconds;
+
         public static void run(int milliseconds, Action callback = null)
         {
             if (!(milliseconds > 0))
@@ -22,6 +25,31 @@ namespace Bee.Timeout
                     callback();
             })
             { IsBackground = true }.Start();
+        }
+
+        public void start(int milliseconds, Action callback = null)
+        {
+            if (!(milliseconds > 0))
+                throw new ArgumentException();
+
+            this.milliseconds = milliseconds;
+            dt = DateTime.Now.AddMilliseconds(this.milliseconds);
+
+            new Thread(() => {
+                while (DateTime.Now < dt)
+                {
+                    // Sleep
+                }
+
+                if (callback != null)
+                    callback();
+            })
+            { IsBackground = true }.Start();
+        }
+
+        public void refresh()
+        {
+            dt = DateTime.Now.AddMilliseconds(milliseconds);
         }
     }
 }
